db3153d113bba5e593ddf8282bfada6f933e5313
gamm-web <prefikes@gmail.com>
2021-03-14 23:23:11 +0800

    new design for home,and create

--- src/views/app/index_.tsx
+++ src/views/app/index_.tsx
@@ -1,229 +1,8 @@
-import React, { Fragment, FormEvent, useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import React, { FormEvent, useEffect, useState } from "react";
 import "./style.css";
 import * as product from "../../db/repositories/product";
-import { useHistory } from "react-router-dom";
 
-const App = () => {
-  const name = 'John Doe'
-  return (
-   <Router>
-
-    <main>
-        <Link to="/add-product">
-            <button>&#43;</button>
-        </Link>
-       
-        <Link to="/">
-            <button>&#128195;</button>
-        </Link>
-        
-    </main>
-    <Switch>
-      <Route path="/" exact component={ProductList} />
-      <Route path="/about/:name" component={About} />
-      <Route path="/edit-product/:id" component={EditProduct} />
-      <Route path="/add-product" component={AddProduct} />
-      <Route path="/contact"  component={Contact} />
-      <Route path="/app1"  component={App1} />
-    </Switch>
-</Router>
-  );
-} 
-
-const AddProduct = () => {
-    const headerTitle = "New Product";
-    const history = useHistory();
-    const [product_name,setProductName] = useState("");
-    const [price,setPrice] = useState("");
-    const [description, setDescription] = useState(""); 
-    const [discount,setDiscount] = useState("");
-
-    const saveProduct = async () => {
-
-        await product.create({ 
-            product_name: product_name, 
-            description: description, 
-            price: price, 
-            discount:discount, 
-            created_date: new Date(), 
-            modified_date: new Date() 
-        });
-
-        history.push("/");
-
-    };
-
-    return (
-        <div className="app">
-            <div className="header">
-                {headerTitle}
-            </div>
-
-            <div className="container">
-
-                <div className="main-wrap">
-
-                    <div className="form-group">
-                        <label>Name</label>
-                        <input 
-                            type="text" 
-                            className="form-control"
-                            onChange={(e) => setProductName(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Description</label>
-                        <textarea  
-                            className="form-control"
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Price</label>
-                        <input 
-                            type="number" 
-                            className="form-control"
-                            onChange={(e) => setPrice(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="form-group">
-                        <label>Discount</label>
-                        <input 
-                            type="number" 
-                            className="form-control"
-                            onChange={(e) => setDiscount(e.target.value)}
-                        />
-                    </div>
-
-                    <button
-                    className="form-submit"
-                    onClick={ () => saveProduct() }
-                    >
-                        Save Product
-                    </button>
-
-                </div>
-                
-
-            </div>
-        </div>
-    )
-
-};
-
-const EditProduct = ({match:{params:{id}}}) => {
-    const headerTitle = "Edit Product";
-
-    return (
-        <div className="app">
-            <div className="header">
-                {headerTitle}
-            </div>
-
-            <div className="container">
-            </div>
-        </div>
-    )
-}
-
-const ProductList = () => {
-    const history = useHistory();
-    const headerTitle = "Product(s)";
-    const [products, setProducts] = useState<Array<product.Product>>([]);
-
-    useEffect(() => {
-        fetchProducts();
-        
-    }, []);
-
-    const fetchProducts = async () => {
-        // clean the products array first
-        setProducts([]);
-
-        // fetch products from repository
-        const _products = await product.all();
-
-        // set products to state
-        setProducts(_products);
-    };
-
-    const remove = async (id: string) => {
-        // clean the products state to prevent user double clicking the delete / edit button
-        setProducts([]);
-
-        // remove product
-        await product.remove(id);
-
-        // fetch again the products
-        fetchProducts();
-    };
-
-    const goToEdit = (id: string) => {
-
-        history.push("/edit-product/" + id);
-
-    };
-
-    return (     
-        <div className="app">
-            <div className="header">
-                {headerTitle}
-            </div>
-
-            <div className="container"> 
-               
-                {/* show if products is empty */}
-                {products.length === 0 ? (
-                    <div className="loading">
-                        <span>Fetching Products ....</span>
-                    </div>
-                ) : null }
-
-                <div className="product-lists">
-
-                {products.map((product, index) => (
-
-<div className="product-item" key={product.id}>
-    
-    <div className="w-80 ">
-       <b className="product-name">{product.product_name}</b>  
-        <p className="product-details">
-        {product.description}
-        </p>
-    </div>
-
-    <div className="w-20">
-
-        <button onClick={ () => { goToEdit(product.id) } } >🖉 Edit</button>
-
-       <div className="product-price">
-       ${product.price}
-       </div>
-         
-    </div>
-  
-  <div className="clear"></div>
-
-</div>                            
-))
-}
-
-                </div>
-                           
-                    
-            </div>
-        </div>
-    );
-
-}; 
-
-
-
-const App1 = () => {    
+const App1 = () => {
     // some needed states
     const [isEditMode, setIsEditMode] = useState(false);
     const [isSubmitting, setIsSubmitting] = useState(false);
@@ -319,6 +98,12 @@ const App1 = () => {
         setSelectedId(id);
     };
 
+    const test = () => {
+
+        return <h1>wew</h1>;
+
+    };
+
     
     return (
         <div className="app">
@@ -402,8 +187,7 @@ const App1 = () => {
                 ) : null}
 
                 {/* products item  */}
-                {
-                products.map((product, index) => (
+                {products.map((product, index) => (
                     <div className="list-item" key={product.id} style={{ marginTop: index > 0 ? "12px" : "" }}>
                         <span className="product_name">{product.product_name}</span>
                         <span className="created_date">On: {product.created_date.toDate().toDateString()}</span>
@@ -416,34 +200,10 @@ const App1 = () => {
                             Delete
                         </span>
                     </div>
-                ))
-                }
-                
+                ))}
             </div>
         </div>
     );
 }; 
 
-const Home = () => (
-  <Fragment>
-    <h1>Home</h1>
-     
-  </Fragment>
-  );
-
-const About = ({match:{params:{name}}}) => (
-  // props.match.params.name
-  <Fragment>
-    <h1>About {name}</h1>
-     
-  </Fragment>
-);
-
-const Contact = () => (
-  <Fragment>
-    <h1>Contact</h1>
-     
-  </Fragment>
-  );
-
-  export default App;
+export default App1;
---
+++ src/views/app/new-product.tsx
@@ -0,0 +1,7 @@
+import React, { FormEvent, useEffect, useState } from "react";
+import "./style.css";
+import * as product from "../../db/repositories/product";
+import * as new_product from "./new-product";
+
+
+
